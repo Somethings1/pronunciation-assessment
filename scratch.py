e47f8c23ec6f77a5c825f1a5db1c0e27d7884b5c
Long <somethings.ttt@gmail.com>
2025-11-20 14:15:44 +0700

    start syl stress processing

--- scratch.py
+++
@@ -1,4 +0,0 @@
-from transformers import Wav2Vec2ForCTC
-model = Wav2Vec2ForCTC.from_pretrained("/Users/admin/Documents/Personal/projects/pronunciation-assessment/models/wav2vec2-large-lv60_phoneme-timit_english_timit-4k", local_files_only=True)
-print(list(model.config.id2label.values())[:100])
-print("Total phones:", len(model.config.id2label))
